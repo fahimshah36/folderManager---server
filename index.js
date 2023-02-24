abf911a46226cc76b8acffeb7bb1c284a59c3583
fahimshah36 <fahimshah860@gmail.com>
2023-02-24 13:23:40 +0600

    building app

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ app.use(bodyParser.json());
 const db =
   "mongodb+srv://admin:adminstart@cluster0.xdutrah.mongodb.net/?retryWrites=true&w=majority";
 
-async function connect() {
-  try {
-    await mongoose.connect(db);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function connect() {
+//   try {
+//     await mongoose.connect(db);
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-connect();
+// connect();
 
 //middlewares
 app.use(cors());
