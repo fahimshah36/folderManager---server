d5d6f9a8f07f1a215f48a633751c000d58a14ea6
fahimshah36 <fahimshah860@gmail.com>
2023-02-24 14:16:47 +0600

    building app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const folderRoutes = require("./router/folders");
+const sampleRoutes = require("./router/routes");
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,26 +12,24 @@ app.use(bodyParser.json());
 const db =
   "mongodb+srv://admin:adminstart@cluster0.xdutrah.mongodb.net/?retryWrites=true&w=majority";
 
-// async function connect() {
-//   try {
-//     await mongoose.connect(db);
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+async function connect() {
+  try {
+    await mongoose.connect(db);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// connect();
+connect();
 
 //middlewares
 app.use(cors());
 
 //routes
-app.use("/", (req, res) => {
-  res.json;
-  ({message: "Hello!!"});
-});
+
 app.use("/api/folder", folderRoutes);
+app.use("/home", sampleRoutes);
 
 //connection
 app.listen(4000, () => {
